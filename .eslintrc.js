2bd3d8f74dece88f31dacadbd511081a8313a527
Hugo Dozois <hugo.dozois@gmail.com>
2015-11-23 12:55:14 -0500

    [updated] eslint config with used globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   extends: 'airbnb',
+  globals: {
+    __DEV__: true,
+    ll: true,
+  },
   plugins: [
     'react',
   ],
   rules: {
     'no-use-before-define': [2, 'nofunc'],
+    'no-unused-vars': [2, { vars: 'all', args: 'none' }],
   },
 };
